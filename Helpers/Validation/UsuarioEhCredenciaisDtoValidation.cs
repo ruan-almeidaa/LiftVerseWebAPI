8ba7494dd530dc02db628c13b7231fb702025818
Ruan <ruanalmeidawegner@gmail.com>
2024-11-13 12:09:16 -0300

    Evolução de validações do fluent

--- Helpers/Validation/UsuarioEhCredenciaisDtoValidation.cs
+++ Helpers/Validation/UsuarioEhCredenciaisDtoValidation.cs
@@ -13,26 +13,26 @@ namespace Helpers.Validation
         public UsuarioEhCredenciaisDtoValidation()
         {
             RuleFor(u => u.Nickname)
-                .NotEmpty()
-                .WithMessage("Campo obrigatório");
+                .NotEmpty().WithMessage("Campo obrigatório")
+                .Length(4, 50).WithMessage("O nickname deve ter entre 2 e 50 caracteres.");
 
             RuleFor(u => u.Nome)
-                .NotEmpty()
-                .WithMessage("Campo obrigatório");            
-            
+                .NotEmpty().WithMessage("Campo obrigatório")
+                .Length(4, 50).WithMessage("O nome deve ter entre 2 e 50 caracteres.")
+                .Matches(@"^[a-zA-ZÀ-ÿ\s]+$").WithMessage("O nome deve conter apenas letras e espaços.");
+
             RuleFor(u => u.Sobrenome)
-                .NotEmpty()
-                .WithMessage("Campo obrigatório");
+                .NotEmpty().WithMessage("Campo obrigatório")
+                .Length(4, 50).WithMessage("O sobrenome deve ter entre 2 e 50 caracteres.")
+                .Matches(@"^[a-zA-ZÀ-ÿ\s]+$").WithMessage("O sobrenome deve conter apenas letras e espaços.");
 
             RuleFor(u => u.Email)
-                .NotEmpty()
-                .EmailAddress()
-                .WithMessage("Campo obrigatório");
-
+                .NotEmpty().WithMessage("Campo obrigatório")
+                .EmailAddress().WithMessage("E-mail inválido.");
 
             RuleFor(u => u.Senha)
-                 .NotEmpty()
-                .WithMessage("Campo obrigatório");
+                .NotEmpty().WithMessage("Campo obrigatório")
+                .Length(10, 50).WithMessage("A senha deve ter entre 10 e 50 caracteres.");
         }
     }
 }

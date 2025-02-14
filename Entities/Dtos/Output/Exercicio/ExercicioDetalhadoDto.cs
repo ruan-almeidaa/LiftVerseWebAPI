bde7a46c3503f174fc93083f052fee524e6e7d44
Ruan <ruanalmeidawegner@gmail.com>
2025-02-13 21:47:32 -0300

    Criado DTO detalhado para Exercicio e simplificado para Variacao.

---
+++ Entities/Dtos/Output/Exercicio/ExercicioDetalhadoDto.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Dtos.Output.VariacaoExercicio;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.Output.Exercicio
+{
+    public class ExercicioDetalhadoDto
+    {
+        public int Id { get; set; }
+        public required string Titulo { get; set; }
+        [MaxLength(2048)]
+        public string? Gif { get; set; }
+        public List<VariacaoExercicioSimplificadoDto>? Variacoes { get; set; }
+        public GrupoMuscular? GrupoMuscular { get; set; }
+
+
+
+    }
+}
---
+++ Entities/Dtos/Output/VariacaoExercicio/VariacaoExercicioSimplificadoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.Output.VariacaoExercicio
+{
+    public class VariacaoExercicioSimplificadoDto
+    {
+        public int Id { get; set; }
+        public required string Titulo { get; set; }
+    }
+}
